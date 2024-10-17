5e54f29bbb5bb095d3047f8d33250b09ce033c55
LEANDRO BUENO DA SILVA <148722703+Le-Bueno@users.noreply.github.com>
2024-10-17 18:10:00 -0300

    Add files via upload

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,31 +1,30 @@
-import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { useState } from 'react';
+import type { FormEvent } from 'react';
+import { Form, Botao, Campo } from './FormVagas.module';
+import { Button } from 'style-components';
 
 type Props = {
-  aoPesquisar: (termo: string) => void
-}
+  aoPesquisar: (termo: string) => void;
+};
 
 const FormVagas = ({ aoPesquisar }: Props) => {
-  const [termo, setTermo] = useState<string>('')
+  const [termo, setTermo] = useState<string>('');
 
   const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    aoPesquisar(termo.toLocaleLowerCase())
-  }
+    e.preventDefault();
+    aoPesquisar(termo.toLocaleLowerCase());
+  };
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <form onSubmit={aoEnviarForm}>
+      <Campo
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
+      <Button type="submit">Pesquisar</Button>
     </form>
-  )
-}
-export default FormVagas
+  );
+};
+
+export default FormVagas;
